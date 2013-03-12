8d8a47faf6b710b81e3666c0ec7687ed94bbd43f
Tom <boooombo@gmail.com>
2013-03-12 23:31:12 +0100

    commit après rollback

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var TileJSONs = [
     'http://a.tiles.mapbox.com/v3/moasth.map-pzgtnf9m.jsonp',
     'http://a.tiles.mapbox.com/v3/moasth.map-czvq0pvt.jsonp',
-    'http://a.tiles.mapbox.com/v3/moasth.chateaux4.jsonp'
+    'http://a.tiles.mapbox.com/v3/moasth.chateaux.jsonp'
 ];
 
 $('#map').mapbox(TileJSONs, function(map, tiledata) {
@@ -20,11 +20,11 @@ $('#map').mapbox(TileJSONs, function(map, tiledata) {
     map.disableLayer('chateaux');
 
     // Set initial latitude, longitude and zoom level
-    map.setCenterZoom({
+    /*map.setCenterZoom({
         lat: 48.583,
         lon: 7.750
-    }, 8);
-    map.setZoomRange(5, 11);
+    }, 8);*/
+    map.setZoomRange(5, 12);
     map.setPanLimits([{ lat: 35.3073, lon: -19.6518 }, { lat: 59.5726, lon: 34.0933}]);
 
     // Set minimum and maximum zoom levels
